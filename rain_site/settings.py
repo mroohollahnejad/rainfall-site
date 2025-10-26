c0b03cee9fe3e4af4f56f59548a638f7f52a4ee2
Mohamad <m.roohollahnejad@chmail.ir>
2025-10-26 13:38:35 +0330

    station table removed

--- rain_site/settings.py
+++ rain_site/settings.py
@@ -79,9 +79,21 @@ WSGI_APPLICATION = 'rain_site.wsgi.application'
 
 # Database (Render PostgreSQL)
 
-DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-}
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- records/apps.py
+++ records/apps.py
@@ -1,12 +1,8 @@
 from django.apps import AppConfig
 
-
 class RecordsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'records'
 
     def ready(self):
-        from .models import Station
-        stations = ['بندرعباس', 'بستک', 'میناب', 'بشاگرد', 'حاجی آباد']
-        for s in stations:
-            Station.objects.get_or_create(name=s)
+        pass  # هیچ کوئری به DB نزنید
